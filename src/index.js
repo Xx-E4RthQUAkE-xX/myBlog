66e957d2d29bd625395e46839fb3fad8306b7657
Xz-xUseKx-zX <kikunetu@gmail.com>
2017-07-14 15:37:53 +0900

    Start using express

---
+++ src/index.js
@@ -0,0 +1,18 @@
+let express = require('express')
+let app = express()
+
+app.get('/', function(req, res) {
+    res.send('Hello World!')
+});
+
+app.get("/name", function(req, res) {
+    res.send("_UseK");
+});
+
+app.get("/show/:id", function(req, res) {
+    res.send("ID: " + req.params.id);
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
+});
