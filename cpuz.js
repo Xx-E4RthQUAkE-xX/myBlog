716e66287b2a78550c6e355c2097cbb14fd3dbc3
Xz-xUseKx-zX <kikunetu@gmail.com>
2017-07-14 14:20:15 +0900

    Cleanup

--- cpuz.js
+++
@@ -1,4 +0,0 @@
-let os = require('os');
-
-console.log(os.freemem());
-console.log(os.freemem() / os.totalmem() * 100);
--- scope-b.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-let a = require('./scope-a');
-
-console.log(a);
