e1ee943b05885d0bb835be7d5a6283fa4f9ad753
Xz-xUseKx-zX <kikunetu@gmail.com>
2017-07-14 16:09:42 +0900

    Implement redirect

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ let express = require('express')
 let app = express()
 
 app.get('/', function(req, res) {
-    res.send('Hello World!')
+    res.redirect(302, '/name');
 });
 
 app.get("/name", function(req, res) {
